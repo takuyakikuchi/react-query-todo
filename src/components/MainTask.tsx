7e4744b4f774b2f678e5f94be3f371140ee3835d
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-03-09 18:23:30 +0900

    feat: add input in MainTask to test memo

--- src/components/MainTask.tsx
+++ src/components/MainTask.tsx
@@ -1,11 +1,20 @@
-import { VFC } from "react"
+import { VFC, useState } from "react"
 import { TaskList } from "./TaskList"
 import { TaskEdit } from "./TaskEdit"
 
 export const MainTask: VFC = () => {
+  const [text, setText] = useState('');
+
   console.log('rendered MainTask')
   return (
     <>
+      <input
+        className="mb-3 px-3 py-2 border border-gray-300"
+        placeholder="input any text"
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
       <p className="mb-10 text-xl font-bold">Tasks</p>
       <div className="grid grid-cols-2 gap-40">
         <TaskList />
